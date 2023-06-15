efabb080731ae0b8f9d7ff392a2fe7704f0d124f
Eugenio Brilhante <eugeniobrilhante@hotmail.com>
2023-06-15 15:19:11 -0300

    Corrigindo erro no JS, deletando pastas JS sem uso no Layout 1, 2 e 3

--- Layout_4/js/scripts.js
+++ Layout_4/js/scripts.js
@@ -1,23 +1,22 @@
 openMenu.addEventListener('click', () => {
     menu.style.display = 'flex'
 
-    menu.style.right = (menu.offsetwidth * -1) + 'px'
-
-    openMenu.style.display = 'none'
+    menu.style.right = (menu.offsetWidth * -1) + 'px'
 
     setTimeout(() => {
         menu.style.opacity = '1'
-    }, 10)
+        menu.style.right = "0"
+        openMenu.style.display = 'none'
+    }, 10);
 })
 
 closeMenu.addEventListener('click', () => {
     menu.style.opacity = '0'
 
-    menu.style.right = (menu.offsetwidth * -1) + 'px'
+    menu.style.right = (menu.offsetWidth * -1) + 'px'
 
     setTimeout(() => {
         menu.removeAttribute('style')
         openMenu.removeAttribute('style')
-    }, 200)
-    
+    }, 200);
 })
